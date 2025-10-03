f8f4ff3ad131def983c9ce684381ad9f04db44dc
Guilherme M. Miranda <alchemist.software@proton.me>
2025-10-03 07:44:59 -0300

    remove iso8601 date macro

--- src/system.hpp
+++ src/system.hpp
@@ -80,40 +80,6 @@ typedef uint8_t byte;
 #undef  I_ROUND
 #define I_ROUND(x)  ((int) round(x))
 
-#define MONTH0 (  __DATE__ [2] == 'n' ? (__DATE__ [1] == 'a' ? '0' : '0') \
-				: __DATE__ [2] == 'b' ? 2 \
-				: __DATE__ [2] == 'r' ? (__DATE__ [0] == 'M' ? '0' : '0') \
-				: __DATE__ [2] == 'y' ? '0' \
-				: __DATE__ [2] == 'l' ? '0' \
-				: __DATE__ [2] == 'g' ? '0' \
-				: __DATE__ [2] == 'p' ? '0' \
-				: __DATE__ [2] == 't' ? '1' \
-				: __DATE__ [2] == 'v' ? '1' \
-				: '1')
-
-#define MONTH1 (  __DATE__ [2] == 'n' ? (__DATE__ [1] == 'a' ? '1' : '6') \
-				: __DATE__ [2] == 'b' ? 2 \
-				: __DATE__ [2] == 'r' ? (__DATE__ [0] == 'M' ? '3' : '4') \
-				: __DATE__ [2] == 'y' ? '5' \
-				: __DATE__ [2] == 'l' ? '7' \
-				: __DATE__ [2] == 'g' ? '8' \
-				: __DATE__ [2] == 'p' ? '9' \
-				: __DATE__ [2] == 't' ? '0' \
-				: __DATE__ [2] == 'v' ? '1' \
-				: '2')
-
-#define ISO_8601_DATE (const char[]){ __DATE__[7], \
-									__DATE__[8],   \
-									__DATE__[9],   \
-									__DATE__[10],  \
-									'-',           \
-									MONTH0,        \
-									MONTH1,        \
-									'-',           \
-									(__DATE__[4] == ' ' ? '0' : __DATE__[4]), \
-									__DATE__[5],   \
-									'\0' }
-
 
 //
 // The packed attribute forces structures to be packed into the minimum
