c4589d9756be66d25f2a44b1d4fa146799c38711
Guilherme M. Miranda <alchemist.software@proton.me>
2026-01-29 11:04:09 -0300

    summy commit

--- scripts/BSP_SplitCostPlot.jl
+++ scripts/BSP_SplitCostPlot.jl
@@ -10,11 +10,22 @@ struct DataVisBSP
   map_name::String
   is_fast::Bool
   split_cost::Int64
-  num_segs::Int64
-  num_subsecs::Int64
-  num_nodes::Int64
-  size_left::Int64
-  size_right::Int64
+  old_vertex::Int64
+  lines::Int64
+  sides::Int64
+  sectors::Int64
+  new_vertex::Int64
+  nodes::Int64
+  subsecs::Int64
+  segs::Int64
+  splits::Int64
+  left_depth::Int64
+  right_depth::Int64
+  average_depth::Float64
+  optimal_depth::Float64
+  tree_balance::Float64
+  worst_case_ratio::Float64
+  tree_quality::Float64
 end
 
 file_path = ARGS[1]
@@ -34,11 +45,22 @@ for row in rows
       row.map_name,
       row.is_fast,
       row.split_cost,
-      row.num_segs,
-      row.num_subsecs,
-      row.num_nodes,
-      row.size_left,
-      row.size_right
+      row.old_vertex,
+      row.lines,
+      row.sides,
+      row.sectors,
+      row.new_vertex,
+      row.nodes,
+      row.subsecs,
+      row.segs,
+      row.splits,
+      row.left_depth,
+      row.right_depth,
+      row.average_depth,
+      row.optimal_depth,
+      row.tree_balance,
+      row.worst_case_ratio,
+      row.tree_quality
     )
   )
 end
@@ -63,60 +85,116 @@ ytick_int = xs -> string.(round.(Int, xs))
 for (map, modes) in maps
   println("[Figure] Setting up $(map) of $(stem)")
 
-  fig = Figure(size=(250 .* (4, 3)))
+  fig = Figure(; size=(2100, 900))
   xt = 0:4:64
 
-  ax = Axis(fig[1, 1], xticks=xt, ytickformat=ytick_int, ylabel="Nodes")
-  ax2 = Axis(fig[2, 1], xticks=xt, ytickformat=ytick_int, ylabel="Subsectors")
-  ax3 = Axis(fig[3, 1], xticks=xt, ytickformat=ytick_int, ylabel="Segments", xlabel="Cost factor of splitting segments")
+  ax_nodes = Axis(fig[1, 1], xticks=xt, ytickformat=ytick_int, ylabel="Nodes")
+  ax_subsecs = Axis(fig[2, 1], xticks=xt, ytickformat=ytick_int, ylabel="Subsectors", xlabel="Cost factor of splitting segments")
 
-  ax4 = Axis(fig[2, 2], xticks=xt, ytickformat=ytick_int, ylabel="Tree depth left", yaxisposition=:right)
-  ax5 = Axis(fig[3, 2], xticks=xt, ytickformat=ytick_int, ylabel="Tree depth right", yaxisposition=:right, xlabel="Cost factor of splitting segments")
+  ax_segs = Axis(fig[1, 2], xticks=xt, ytickformat=ytick_int, ylabel="Segments")
+  ax_splits = Axis(fig[2, 2], xticks=xt, ytickformat=ytick_int, ylabel="Splits", xlabel="Cost factor of splitting segments")
 
-  linkxaxes!(ax, ax2, ax3)
-  linkxaxes!(ax4, ax5)
+  ax_left_depth = Axis(fig[1, 3], xticks=xt, ytickformat=ytick_int, ylabel="Tree depth left")
+  ax_right_depth = Axis(fig[2, 3], xticks=xt, ytickformat=ytick_int, ylabel="Tree depth right", xlabel="Cost factor of splitting segments")
+
+  ax_average_depth = Axis(fig[1, 4], xticks=xt, ytickformat=ytick_int, ylabel="Average tree depth until leaf")
+  ax_optimal_depth = Axis(fig[2, 4], xticks=xt, ytickformat=ytick_int, ylabel="Optimal EPL tree depth", xlabel="Cost factor of splitting segments")
+
+  ax_tree_balance = Axis(fig[1, 5], xticks=xt, ylabel="Tree balance")
+  ax_tree_quality = Axis(fig[2, 5], xticks=xt, ylabel="Tree quality", xlabel="Cost factor of splitting segments")
+
+  linkxaxes!(ax_nodes, ax_subsecs)
+  linkxaxes!(ax_segs, ax_splits)
+  linkxaxes!(ax_left_depth, ax_right_depth)
+  linkxaxes!(ax_average_depth, ax_optimal_depth)
+  linkxaxes!(ax_tree_balance, ax_tree_quality)
 
   # Start ploting
   data_normal = modes[false]
   data_fast = modes[true]
   x = 1:length(data_normal)
 
-  y_nodes = getfield.(data_normal, :num_nodes)
-  y_subsecs = getfield.(data_normal, :num_subsecs)
-  y_segs = getfield.(data_normal, :num_segs)
-  y_size_left = getfield.(data_normal, :size_left)
-  y_size_right = getfield.(data_normal, :size_right)
+  # normal
+  y_nodes = getfield.(data_normal, :nodes)
+  y_subsecs = getfield.(data_normal, :subsecs)
 
-  y_nodes_fast = getfield.(data_fast, :num_nodes)
-  y_subsecs_fast = getfield.(data_fast, :num_subsecs)
-  y_segs_fast = getfield.(data_fast, :num_segs)
-  y_size_left_fast = getfield.(data_fast, :size_left)
-  y_size_right_fast = getfield.(data_fast, :size_right)
+  y_segs = getfield.(data_normal, :segs)
+  y_splits = getfield.(data_normal, :splits)
 
-  lines!(ax, x, y_nodes; color=:dodgerblue, linestyle=:solid, alpha=0.50)
-  scatter!(ax, x, y_nodes; color=:dodgerblue, marker=:circle)
-  lines!(ax, x, y_nodes_fast; color=:dodgerblue, linestyle=:dash, alpha=0.50)
-  scatter!(ax, x, y_nodes_fast; color=:dodgerblue, marker=:utriangle)
+  y_left_depth = getfield.(data_normal, :left_depth)
+  y_right_depth = getfield.(data_normal, :right_depth)
 
-  lines!(ax2, x, y_subsecs; color=:seagreen, linestyle=:solid, alpha=0.50)
-  scatter!(ax2, x, y_subsecs; color=:seagreen, marker=:circle)
-  lines!(ax2, x, y_subsecs_fast; color=:seagreen, linestyle=:dash, alpha=0.50)
-  scatter!(ax2, x, y_subsecs_fast; color=:seagreen, marker=:utriangle)
+  y_average_depth = getfield.(data_normal, :average_depth)
+  y_optimal_depth = getfield.(data_normal, :optimal_depth)
 
-  lines!(ax3, x, y_segs; color=:firebrick, linestyle=:solid, alpha=0.50)
-  scatter!(ax3, x, y_segs; color=:firebrick, marker=:circle)
-  lines!(ax3, x, y_segs_fast; color=:firebrick, linestyle=:dash, alpha=0.50)
-  scatter!(ax3, x, y_segs_fast; color=:firebrick, marker=:utriangle)
+  y_tree_balance = getfield.(data_normal, :tree_balance)
+  y_tree_quality = getfield.(data_normal, :tree_quality)
 
-  lines!(ax4, x, y_size_left; color=:purple, linestyle=:solid, alpha=0.50)
-  scatter!(ax4, x, y_size_left; color=:purple, marker=:circle)
-  lines!(ax4, x, y_size_left_fast; color=:purple, linestyle=:dash, alpha=0.50)
-  scatter!(ax4, x, y_size_left_fast; color=:purple, marker=:utriangle)
+  # fast
+  y_nodes_fast = getfield.(data_fast, :nodes)
+  y_subsecs_fast = getfield.(data_fast, :subsecs)
 
-  lines!(ax5, x, y_size_right; color=:darkgoldenrod, linestyle=:solid, alpha=0.50)
-  scatter!(ax5, x, y_size_right; color=:darkgoldenrod, marker=:circle)
-  lines!(ax5, x, y_size_right_fast; color=:darkgoldenrod, linestyle=:dash, alpha=0.50)
-  scatter!(ax5, x, y_size_right_fast; color=:darkgoldenrod, marker=:utriangle)
+  y_segs_fast = getfield.(data_fast, :segs)
+  y_splits_fast = getfield.(data_fast, :splits)
+
+  y_left_depth_fast = getfield.(data_fast, :left_depth)
+  y_right_depth_fast = getfield.(data_fast, :right_depth)
+
+  y_average_depth_fast = getfield.(data_fast, :average_depth)
+  y_optimal_depth_fast = getfield.(data_fast, :optimal_depth)
+
+  y_tree_balance_fast = getfield.(data_fast, :tree_balance)
+  y_tree_quality_fast = getfield.(data_fast, :tree_quality)
+
+  lines!(ax_nodes, x, y_nodes; color=:dodgerblue, linestyle=:solid, alpha=0.50)
+  scatter!(ax_nodes, x, y_nodes; color=:dodgerblue, marker=:circle)
+  lines!(ax_nodes, x, y_nodes_fast; color=:dodgerblue, linestyle=:dash, alpha=0.50)
+  scatter!(ax_nodes, x, y_nodes_fast; color=:dodgerblue, marker=:utriangle)
+
+  lines!(ax_subsecs, x, y_subsecs; color=:seagreen, linestyle=:solid, alpha=0.50)
+  scatter!(ax_subsecs, x, y_subsecs; color=:seagreen, marker=:circle)
+  lines!(ax_subsecs, x, y_subsecs_fast; color=:seagreen, linestyle=:dash, alpha=0.50)
+  scatter!(ax_subsecs, x, y_subsecs_fast; color=:seagreen, marker=:utriangle)
+
+  lines!(ax_segs, x, y_segs; color=:firebrick, linestyle=:solid, alpha=0.50)
+  scatter!(ax_segs, x, y_segs; color=:firebrick, marker=:circle)
+  lines!(ax_segs, x, y_segs_fast; color=:firebrick, linestyle=:dash, alpha=0.50)
+  scatter!(ax_segs, x, y_segs_fast; color=:firebrick, marker=:utriangle)
+
+  lines!(ax_splits, x, y_splits; color=:maroon, linestyle=:solid, alpha=0.50)
+  scatter!(ax_splits, x, y_splits; color=:maroon, marker=:circle)
+  lines!(ax_splits, x, y_splits_fast; color=:maroon, linestyle=:dash, alpha=0.50)
+  scatter!(ax_splits, x, y_splits_fast; color=:maroon, marker=:utriangle)
+
+  lines!(ax_left_depth, x, y_left_depth; color=:purple, linestyle=:solid, alpha=0.50)
+  scatter!(ax_left_depth, x, y_left_depth; color=:purple, marker=:circle)
+  lines!(ax_left_depth, x, y_left_depth_fast; color=:purple, linestyle=:dash, alpha=0.50)
+  scatter!(ax_left_depth, x, y_left_depth_fast; color=:purple, marker=:utriangle)
+
+  lines!(ax_right_depth, x, y_right_depth; color=:darkgoldenrod, linestyle=:solid, alpha=0.50)
+  scatter!(ax_right_depth, x, y_right_depth; color=:darkgoldenrod, marker=:circle)
+  lines!(ax_right_depth, x, y_right_depth_fast; color=:darkgoldenrod, linestyle=:dash, alpha=0.50)
+  scatter!(ax_right_depth, x, y_right_depth_fast; color=:darkgoldenrod, marker=:utriangle)
+
+  lines!(ax_average_depth, x, y_average_depth; color=:teal, linestyle=:solid, alpha=0.50)
+  scatter!(ax_average_depth, x, y_average_depth; color=:teal, marker=:circle)
+  lines!(ax_average_depth, x, y_average_depth_fast; color=:teal, linestyle=:dash, alpha=0.50)
+  scatter!(ax_average_depth, x, y_average_depth_fast; color=:teal, marker=:utriangle)
+
+  lines!(ax_optimal_depth, x, y_optimal_depth; color=:navajowhite3, linestyle=:solid, alpha=0.50)
+  scatter!(ax_optimal_depth, x, y_optimal_depth; color=:navajowhite3, marker=:circle)
+  lines!(ax_optimal_depth, x, y_optimal_depth_fast; color=:navajowhite3, linestyle=:dash, alpha=0.50)
+  scatter!(ax_optimal_depth, x, y_optimal_depth_fast; color=:navajowhite3, marker=:utriangle)
+
+  lines!(ax_tree_balance, x, y_tree_balance; color=:seagreen, linestyle=:solid, alpha=0.50)
+  scatter!(ax_tree_balance, x, y_tree_balance; color=:seagreen, marker=:circle)
+  lines!(ax_tree_balance, x, y_tree_balance_fast; color=:seagreen, linestyle=:dash, alpha=0.50)
+  scatter!(ax_tree_balance, x, y_tree_balance_fast; color=:seagreen, marker=:utriangle)
+
+  lines!(ax_tree_quality, x, y_tree_quality; color=:steelblue, linestyle=:solid, alpha=0.50)
+  scatter!(ax_tree_quality, x, y_tree_quality; color=:steelblue, marker=:circle)
+  lines!(ax_tree_quality, x, y_tree_quality_fast; color=:steelblue, linestyle=:dash, alpha=0.50)
+  scatter!(ax_tree_quality, x, y_tree_quality_fast; color=:steelblue, marker=:utriangle)
 
   # End ploting
   build_normal = [
@@ -129,11 +207,11 @@ for (map, modes) in maps
     MarkerElement(color=:black, marker=:utriangle, strokecolor=:black)
   ]
 
-  Legend(fig[1, 2], [build_normal, build_fast], ["Normal build", "Fast build"])
+  # Legend(fig[1, 2], [build_normal, build_fast], ["Normal build", "Fast build"])
   Label(fig[0, :], "ELFBSP Segment Split Cost Analysis - $(stem) $(map)", justification=:center, font=:bold, fontsize=20)
 
-  colsize!(fig.layout, 1, Relative(0.50))
-  colsize!(fig.layout, 2, Relative(0.50))
+  # colsize!(fig.layout, 1, Relative(0.50))
+  # colsize!(fig.layout, 2, Relative(0.50))
 
   save(joinpath(out_dir, "$(stem) $(map).png"), fig)
 end
