0a6b98881b28f3018011e3680735d7f39ecbd1bc
Guilherme M. Miranda <alchemist.software@proton.me>
2026-02-08 12:49:28 -0300

    added copyright header

--- src/bsp.cpp
+++ src/bsp.cpp
@@ -1,3 +1,26 @@
+//------------------------------------------------------------------------------
+//
+//  ELFBSP
+//
+//------------------------------------------------------------------------------
+//
+//  Copyright 2025-2026 Guilherme Miranda
+//  Copyright 2000-2018 Andrew Apted, et al
+//  Copyright 1994-1998 Colin Reed
+//  Copyright 1997-1998 Lee Killough
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//------------------------------------------------------------------------------
+
 #include <algorithm>
 
 #include "core.hpp"
